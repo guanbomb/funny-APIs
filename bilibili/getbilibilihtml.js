0fde655450a67499a11ce7741f88d233f2dafa43
youngerheart <435495400@qq.com>
2014-08-08 22:38:14 +0800

    很不幸，直接使用ajax会跨域，即使js文件资源和后台文件在一个域，因为用户使用的域名和js文件所在域名不同，js请求文件时还是跨域了，改用jsonp形式请求，只能用get了感觉不爽。。。正在寻求方法

--- bilibili/getbilibilihtml.js
+++ bilibili/getbilibilihtml.js
@@ -1,45 +1,44 @@
 function val(obj){return document.getElementById(obj)};
 var obj = val("bilibili_card");
 if(obj&&obj.getAttribute('uid')){
-	url = obj.getAttribute('uid')
-	var XMLHttpReq;  
-	function createXMLHttpRequest() {  
-	    try {  
-	        XMLHttpReq = new ActiveXObject("Msxml2.XMLHTTP");//	IE高版本创建XMLHTTP  
-	    }  
-	    catch(E) {  
-	        try {  
-	            XMLHttpReq = new ActiveXObject("MicrosoftXMLHTTP");//		IE低版本创建XMLHTTP  
-	        }  
-	        catch(E) {  
-	            XMLHttpReq = new XMLHttpRequest();//	兼容非IE浏览器，直接创建XMLHTTP对象  
-	        }  
-	    }  
-	  
-	}  
-	function sendAjaxRequest(url,data) {  
-	    createXMLHttpRequest();//创建XMLHttpRequest对象  
-	    XMLHttpReq.open('post', url, true); 
-	    XMLHttpReq.setRequestHeader("Content-Type","application/x-www-form-urlencoded;charset=UTF-8")
-	    XMLHttpReq.send('uid='+data);   
-	    XMLHttpReq.onreadystatechange = processResponse; //指定响应函数  
-	}
-	function processResponse(){
-		if (XMLHttpReq.readyState == 4&&XMLHttpReq.status == 200){
-				var str = XMLHttpReq.responseText;
-				eval(str);
-		}
-	}
-	sendAjaxRequest("http://uquweb.com/api/bilibili/getbilibilihtml.php",obj.getAttribute('uid'))
 
-	
-	
-	
+	url = obj.getAttribute('uid')
+	//ajax请求是跨域的，不可行！
+	// var XMLHttpReq;  
+	// function createXMLHttpRequest() {  
+	//     try {  
+	//         XMLHttpReq = new ActiveXObject("Msxml2.XMLHTTP");//	IE高版本创建XMLHTTP  
+	//     }  
+	//     catch(E) {  
+	//         try {  
+	//             XMLHttpReq = new ActiveXObject("MicrosoftXMLHTTP");//		IE低版本创建XMLHTTP  
+	//         }  
+	//         catch(E) {  
+	//             XMLHttpReq = new XMLHttpRequest();//	兼容非IE浏览器，直接创建XMLHTTP对象  
+	//         }  
+	//     }  
+	  
+	// }  
+	// function sendAjaxRequest(url,data) {  
+	//     createXMLHttpRequest();//创建XMLHttpRequest对象  
+	//     XMLHttpReq.open('post', url, true); 
+	//     XMLHttpReq.setRequestHeader("Content-Type","application/x-www-form-urlencoded;charset=UTF-8")
+	//     XMLHttpReq.send('uid='+data);   
+	//     XMLHttpReq.onreadystatechange = processResponse; //指定响应函数  
+	// }
+	// function processResponse(){
+	// 	if (XMLHttpReq.readyState == 4&&XMLHttpReq.status == 200){
+	// 			var str = XMLHttpReq.responseText;
+	// 			eval(str);
+	// 	}
+	// }
+	// sendAjaxRequest("http://uquweb.com/api/bilibili/getbilibilihtml.php",obj.getAttribute('uid'))
+		document.write("<script src=\"http://uquweb.com/api/bilibili/getbilibilihtml.php?uid="+obj.getAttribute('uid')+"\"></script>");
 }
 else{
 	alert("哔哩哔哩模块出错！请检查代码");
 }
 function ShowCard(json){
-	var name="<div class=\"bilibili-card user-card\"><div class=\"header\"><a class=\"avatar\" href=\"http://space.bilibili.tv/"+json.mid+"/info.html\" target=\"_top\"><img src=\""+json.face+"\"><strong>"+json.name+"</strong><span>"+json.place+"</span></a><a class=\"button\" href=\"http://space.bilibili.tv/"+json.mid+"/info.html\" target=\"_top\">进入Ta的空间</a></div><ul class=\"status\"><li><a href=\"http://space.bilibili.com/"+json.mid+"/fans.html\" target=\"_top\"><strong>"+json.fans+"</strong>粉丝</a></li><li><a href=\"http://space.bilibili.com/"+json.mid+"/follow.html\" target=\"_top\"><strong>"+json.attention+"</strong>关注</a></li><li><a href=\"http://space.bilibili.com/"+json.mid+"\" target=\"_top\"><strong>"+json.article+"</strong>投稿</a></li></ul><div class=\"footer\"><a href=\"http://www.bilibili.tv\" target=\"_top\">Move to BiliBili.</a>&nbsp;&nbsp;&nbsp;Powered By <a href=\"http://uquweb.com\">Younger</a></div></div>";
+	var name="<div class=\"bilibili-card user-card\"><div class=\"header\"><a class=\"avatar\" href=\"http://space.bilibili.tv/"+json.mid+"/info.html\" target=\"_top\"><img src=\""+json.face+"\"><strong>"+json.name+"</strong><span>"+json.place+"</span></a><a class=\"button\" href=\"http://space.bilibili.tv/"+json.mid+"\" target=\"_top\">进入Ta的空间</a></div><ul class=\"status\"><li><a href=\"http://space.bilibili.com/"+json.mid+"/fans.html\" target=\"_top\"><strong>"+json.fans+"</strong>粉丝</a></li><li><a href=\"http://space.bilibili.com/"+json.mid+"/follow.html\" target=\"_top\"><strong>"+json.attention+"</strong>关注</a></li><li><a href=\"http://space.bilibili.com/"+json.mid+"\" target=\"_top\"><strong>"+json.article+"</strong>投稿</a></li></ul><div class=\"footer\"><a href=\"http://www.bilibili.tv\" target=\"_top\">Move to BiliBili.</a>&nbsp;&nbsp;&nbsp;Powered By <a href=\"http://uquweb.com\">Younger</a></div></div>";
 	obj.innerHTML=name+"<link rel=\"stylesheet\" type=\"text/css\" href=\"http://uquweb.com/api/bilibili/bilibilicard.css\">";
 }
